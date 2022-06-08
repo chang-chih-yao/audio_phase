517f7a9994041d021712a3324f6070b39fd63b49
cychang_RTK_NB <m900054@gmail.com>
2022-06-08 19:08:10 +0800

    Update my_script.py

--- my_script.py
+++ my_script.py
@@ -5,9 +5,6 @@ if os.path.exists('gen_sv_test.py'):
     os.remove('gen_sv_test.py')
 
 src=r'C:\Users\cychang\Documents\GitHub\audio_phase\gen_sv_test.py'
-des=r'C:\Users\cychang\Documents\RTK\audio_data_phase\5575\gen\gen_sv_test.py'
-
-shutil.copy(src, des)
-
+shutil.copy(src, os.getcwd())
 print('------------------------------------------')
 os.system('python gen_sv_test.py')
